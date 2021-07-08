6e35a8a4708c0f079b73e8f5cee6a664b75d3a98
Arsenii Tverdokhleb <arsenii.tverdokhleb@extendaretail.com>
2021-07-08 14:45:38 +0300

    test

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,17 +4,11 @@ import { Injectable } from '@nestjs/common';
 export class TodoService {
   private readonly cats: any[] = [];
 
-                          create(cat: any) { 
-
-                            
+  create(cat: any) {
     this.cats.push(cat);
   }
 
-            findAll():             any[] {
-
- 
-
-
+  findAll(): any[] {
     return this.cats;
   }
 }
